5a61c6d2e5b5ac4a27f56e1f1cc90f82d137dcfd
Sahan1103 <madurangasahan576@gmail.com>
2025-11-02 20:44:17 +0530

    Update Student.java

--- Tuition Management System/src/tuitionapp/model/Student.java
+++ Tuition Management System/src/tuitionapp/model/Student.java
@@ -1,4 +1,12 @@
 package tuitionapp.model;
 
 public class Student {
+	private String studentId;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String enrolledCourse;
+    private double totalFees;
+    private double paidAmount;
 }
